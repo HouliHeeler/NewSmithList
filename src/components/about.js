c634b201e4701b42bfacbe91af841948adaab7fd
HouliHeeler <luke_houlihan@hotmail.com>
2022-04-30 22:01:06 -0700

    lesson one solo project

---
+++ src/components/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function About() {
+    return(
+        <div className="about">
+            <h2>About</h2>
+            <p>I am a Software Engineer with a particular interest in working with React, and building intuitive user experiences. Putting out a high-quality, impressive product, and challenging myself every day are what bring me the most joy in the tech industry.</p>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Footer() {
+    return(
+        <div className="footer">
+            <img alt="LinkedIn"/>
+            <img alt="GitHub"/>
+            <img alt="Twitter"/>
+            <img alt="Facebook"/>
+        </div>
+    )
+}
+
+export default Footer;
