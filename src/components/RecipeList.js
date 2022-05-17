75a8c55ce877f0c295dc92cf852ba228a8eb3b56
HouliHeeler <luke_houlihan@hotmail.com>
2022-05-17 16:46:57 -0700

    add search bar

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -3,13 +3,18 @@ import React from "react"
 export default function RecipeList() {
     return(
         <div className="recipe--list">
-            <label for="cuisines"></label>
-            <select name="cuisines" id="cuisines">
-            <option value="AroundtheWorld">Around the World</option>
-            <option value="Indian">Indian</option>
-            <option value="Thai">Thai</option>
-            <option value="Mexican">Mexican</option>
-            </select> 
+            <div className="dropdown">
+                <label for="cuisines"></label>
+                <select name="cuisines" id="cuisines" style={{width: "22vw"}}>
+                <option value="AroundtheWorld">Around the World</option>
+                <option value="Indian">Indian</option>
+                <option value="Thai">Thai</option>
+                <option value="Mexican">Mexican</option>
+                </select>
+            </div>
+            <div className="search">
+                <input type="text" placeholder="Search.." style={{width: "22vw"}}></input>  
+            </div>
         </div>
     )
 }
