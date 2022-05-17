3c0fe15ce8fb3dd73e56bffd00d31e24a98d20ed
HouliHeeler <luke_houlihan@hotmail.com>
2022-05-16 18:49:00 -0700

    fresh start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
 import React from "react"
-import Header from "./components/Header"
-import Location from "./components/Location"
-import data from "./data"
 import "./App.css"
 
 export default function App() {
-  const card = data.map(item => {
-    return(
-      <Location 
-        key = {item.id}
-        item = {item}
-      />
-    )
-  })
-  return (
-    <div>
-      <Header />
-      <section>
-        {card}
-      </section>
-    </div>
-  )
+  
 }
