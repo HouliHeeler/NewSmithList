3926fc043120763960c536886f4f204ecfc5db60
HouliHeeler <luke_houlihan@hotmail.com>
2022-05-18 16:20:20 -0700

    style search bar

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -6,14 +6,14 @@ export default function RecipeList() {
             <div className="dropdown">
                 <label for="cuisines"></label>
                 <select name="cuisines" id="cuisines" style={{width: "22vw"}}>
-                <option value="AroundtheWorld">Around the World</option>
-                <option value="Indian">Indian</option>
-                <option value="Thai">Thai</option>
-                <option value="Mexican">Mexican</option>
+                    <option value="AroundtheWorld">Around the World</option>
+                    <option value="Indian">Indian</option>
+                    <option value="Thai">Thai</option>
+                    <option value="Mexican">Mexican</option>
                 </select>
             </div>
             <div className="search">
-                <input type="text" placeholder="Search.." style={{width: "22vw"}}></input>  
+                <input placeholder="Search..." type="search" required></input>
             </div>
         </div>
     )
